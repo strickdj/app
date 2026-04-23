9b34496b87d11bf3fb16877d94a1babf0ad9148b
Daris Strickland <daris.strickland@gmail.com>
2026-04-22 20:38:02 -0400

    stuff

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware(['auth'])->group(function (): void {
 });
 
 Route::get('/hello', function () {
-    return 'Hello World!';
+    return 'Hello World!!';
 });
 
 require __DIR__.'/settings.php';
